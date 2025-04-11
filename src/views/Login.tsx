df47bb3d5c7910fe7865bca0d0225cc2d822fe2c
AFK247 <asif@eagle3dstreaming.com>
2025-04-11 14:31:00 +0600

    fix: update quickBook login handler to open URL in the same tab

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -96,9 +96,7 @@ const LoginV2 = ({ mode }: { mode: SystemMode }) => {
 
     const authUrl = `https://appcenter.intuit.com/connect/oauth2?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&state=1234`
 
-    console.log(authUrl)
-
-    window.open(authUrl)
+    window.open(authUrl, '_self')
   }
 
   return (
